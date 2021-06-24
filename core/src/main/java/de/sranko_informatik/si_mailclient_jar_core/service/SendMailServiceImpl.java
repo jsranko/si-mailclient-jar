d6a6ac670b0bda390cbc6b5493271cae2cc8cb0d
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-06-24 11:37:31 +0200

    logger.info after mail send

--- core/src/main/java/de/sranko_informatik/si_mailclient_jar_core/service/SendMailServiceImpl.java
+++ core/src/main/java/de/sranko_informatik/si_mailclient_jar_core/service/SendMailServiceImpl.java
@@ -2,6 +2,8 @@ package de.sranko_informatik.si_mailclient_jar_core.service;
 
 import de.sranko_informatik.si_mailclient_jar_core.model.Mail;
 import de.sranko_informatik.si_mailclient_jar_core.model.MailResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import javax.mail.internet.MimeMessage;
 
 @Service
 public class SendMailServiceImpl implements SendMailService{
+
+    Logger logger = LoggerFactory.getLogger(SendMailServiceImpl.class);
 
     private final JavaMailSender javaMailSender;
 
@@ -24,11 +28,21 @@ public class SendMailServiceImpl implements SendMailService{
         MimeMessage msg = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 
-        helper.setTo(mail.getRecipients());
-        helper.setCc(mail.getRecipientsCC());
-        helper.setBcc(mail.getRecipientsBCC());
-        helper.setFrom(mail.getFrom());
-        helper.setSubject(mail.getSubject());
+        if (mail.getRecipients() != null) {
+            helper.setTo(mail.getRecipients());
+        }
+        if (mail.getRecipientsCC() != null) {
+            helper.setCc(mail.getRecipientsCC());
+        }
+        if (mail.getRecipientsBCC() != null) {
+            helper.setBcc(mail.getRecipientsBCC());
+        }
+        if (mail.getFrom() != null) {
+            helper.setFrom(mail.getFrom());
+        }
+        if (mail.getSubject() != null) {
+            helper.setSubject(mail.getSubject());
+        }
         if (mail.isHTML()){
             helper.setText(mail.getMessage().getText(), true);
         } else {
@@ -51,8 +65,8 @@ public class SendMailServiceImpl implements SendMailService{
             }
         }
 
-
         javaMailSender.send(msg);
+        logger.info("Email successful send. ".concat(mail.toString()));
     }
 
 }
